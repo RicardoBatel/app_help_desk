fe7a910194bbea2c5d55aabbcd085cbd55aa7379
RIcardo Batel <ricardo.batel@hotmail.com>
2022-11-16 17:36:32 -0300

    consultando chamados

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -1,4 +1,23 @@
 <? require_once "validador_acesso.php" ?>
+<?php
+
+  //chamados
+  $chamados = [];
+
+  //abrir arquivo.hd
+  $arquivo = fopen('arquivo.hd', 'r');
+
+  //enquanto houver registros (linhas) a serem recuperados
+  while(!feof($arquivo)) { //testa pelo fim de um arquivo
+    //linha
+    $registro = fgets($arquivo);
+    $chamados[] = $registro;
+  }
+
+  //fechar o arquivo
+  fclose($arquivo);
+
+?>
 
 <html>
   <head>
@@ -40,24 +59,27 @@
             </div>
             
             <div class="card-body">
+
+              <? foreach($chamados as $chamado) { ?>
               
+              <?php
+
+                $chamado_dados = explode('#', $chamado);
+
+                if(count($chamado_dados) < 3) {
+                  continue;
+                };
+
+              ?>
               <div class="card mb-3 bg-light">
                 <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
-
+                  <h5 class="card-title"><?=$chamado_dados[0]?></h5>
+                  <h6 class="card-subtitle mb-2 text-muted"><?=$chamado_dados[1]?></h6>
+                  <p class="card-text"><?=$chamado_dados[2]?></p>
                 </div>
               </div>
-
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
-
-                </div>
-              </div>
+              
+              <? } ?>
 
               <div class="row mt-5">
                 <div class="col-6">
